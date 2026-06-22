8843a3d115902bfe0bbe30793c00d07377f4708f
hiop <hiopvkashu@gmail.com>
2026-06-22 14:18:50 +0500

    mod

---
+++ MeleeMultiplier.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.Utils;
+using Path = System.IO.Path;
+
+namespace DynamicFleaNamespace;
+
+[Injectable(InjectionType = InjectionType.Singleton)]
+public class MeleeMultiplier(
+    ISptLogger<MeleeMultiplier> logger
+)
+{
+    public static readonly string ModName = "MeleeMultiplier";
+    private static readonly string _configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "user", "mods", "MeleeMultiplier", "Config", "MeleeMultiplierConfig.json5");
+    private MeleeMultiplierConfig? _config;
+    
+    /**
+     * Load config or create new one with default value
+     */
+    public void LoadDynamicFleaConfig()
+    {
+        try
+        {
+            if (File.Exists(_configPath))
+            {
+                var jsonContent = File.ReadAllText(_configPath);
+                var loadedConfig = JsonSerializer.Deserialize<MeleeMultiplierConfig>(jsonContent,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                        ReadCommentHandling = JsonCommentHandling.Skip,
+                        AllowTrailingCommas = true
+                    });
+
+                _config = loadedConfig;
+            }
+            else
+            {
+                _config = new MeleeMultiplierConfig()
+                {
+                    MeleeDamageMultiplier = 10
+                };
+                
+                var fileInfo = new FileInfo(_configPath);
+                fileInfo.Directory?.Create();
+
+                var jsonString = JsonSerializer.Serialize(_config, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(_configPath, jsonString);
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.Error($"{ModName}: error on load config", ex);
+            throw;
+        }
+    }
+
+    public int GetMeleeDamageMultiplier()
+    {
+        return _config!.MeleeDamageMultiplier;
+    }
+}
+
+public class MeleeMultiplierConfig
+{
+    [JsonPropertyName("meleeDamageMultiplier")] 
+    public int MeleeDamageMultiplier { get; set; } = 1;
+}
---
+++ MeleeMultiplierOnLoad.cs
@@ -0,0 +1,53 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Services;
+using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
+
+namespace DynamicFleaNamespace;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "hiop.melee.multiplier";
+    public override string Name { get; init; } = MeleeMultiplier.ModName;
+    public override string Author { get; init; } = "HioP";
+    public override List<string>? Contributors { get; init; }
+    public override Version Version { get; init; } = new("1.0.0");
+    public override Range SptVersion { get; init; } = new("~4.0.7");
+    
+    
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; }
+    public override string? License { get; init; } = "MIT";
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader)]
+public class MeleeMultiplierOnLoad(
+    ISptLogger<MeleeMultiplierOnLoad> logger,
+    MeleeMultiplier meleeMultiplier,
+    DatabaseService databaseService
+    ) : IOnLoad
+{
+    public Task OnLoad()
+    {
+        meleeMultiplier.LoadDynamicFleaConfig();
+        foreach (KeyValuePair<MongoId,TemplateItem> keyValuePair in databaseService.GetTemplates().Items)
+        {
+            if (keyValuePair.Value.Properties.KnifeHitStabDam != null)
+            {
+                keyValuePair.Value.Properties.KnifeHitStabDam *= meleeMultiplier.GetMeleeDamageMultiplier();
+                keyValuePair.Value.Properties.KnifeHitSlashDam *= meleeMultiplier.GetMeleeDamageMultiplier();
+            }
+        }
+        
+        logger.Success($"[{MeleeMultiplier.ModName}] damage multiplier x{meleeMultiplier.GetMeleeDamageMultiplier()}!");
+        
+        return Task.CompletedTask;
+    }
+}
